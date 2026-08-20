fd57522004e370b319fc88bc61d52a296df4ebca
Naseem Haider Malik <naseemhaider086@gmail.com>
2026-08-21 02:40:08 +0500

    Updated first.py with string method practice

---
+++ first.py
@@ -0,0 +1,10 @@
+# int data type
+number = 23
+print(type(number))
+print(number)
+
+# string
+first_name = "Naseem Haider"
+print(first_name.capitalize())
+print(first_name.casefold())
+print(first_name.center(23, "_")) # center method
